04090bf200c4f7da11d639cda5982331000708cb
Andrew <andrewjcw88@gmail.com>
2018-03-27 07:26:17 +1100

    imported base classes and started on BalloonLevel

--- core/src/com/mygdx/game/BalloonLevel.java
+++ core/src/com/mygdx/game/BalloonLevel.java
@@ -1,33 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
-public class BalloonLevel extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class BalloonLevel extends BaseScreen {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private BaseActor background;
+
+    private float spawnTimer;
+    private float spawnInterval;
+
+    private int popped;
+    private int escaped;
+    private int clickCount;
+
+    private Label poppedLabel;
+    private Label escapedLabel;
+    private Label hitRatioLabel;
+
+    // Game world dimensions
+    final int mapWidth = 640;
+    final int mapHeight = 480;
+
+    public BalloonLevel(Game g) {
+        super(g);
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
 }
