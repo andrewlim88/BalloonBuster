b9b38df3c70ec40a7db792ff1d0303f962d2bb94
Andrew <andrewjcw88@gmail.com>
2018-03-26 18:42:42 +1100

    initialise...

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.BalloonLevel;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BalloonLevel(), config);
+	}
+}
